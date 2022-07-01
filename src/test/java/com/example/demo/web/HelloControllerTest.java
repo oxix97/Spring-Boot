5ba0f23d0678cfa8e940e3e32c505c1792ed133b
username <ieejo716@naver.com>
2022-07-01 15:21:39 +0900

    스프링부트 부트와 AWS로 혼자 구현하는 웹 서비스 2-2

--- src/test/java/com/example/demo/web/HelloControllerTest.java
+++ src/test/java/com/example/demo/web/HelloControllerTest.java
@@ -1,7 +1,6 @@
-package com.example.demo;
+package com.example.demo.web;
 
 
-import com.example.demo.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +8,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -25,6 +24,20 @@ public class HelloControllerTest {
         String hello = "hello";
 
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    @Test
+    public void printHelloDto() throws Exception {
+        String name = "Hello";
+        int amount = 1000;
+
+        mvc.perform(
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 
 }
